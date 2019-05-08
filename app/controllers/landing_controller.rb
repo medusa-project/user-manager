45bcf8d735becff437edca52871a797eed54da76
Alex Dolski <alexd@n-ary.net>
2019-05-08 10:14:59 -0500

    Replace LandingController with a redirect to /users

--- app/controllers/landing_controller.rb
+++
@@ -1,9 +0,0 @@
-class LandingController < ApplicationController
-
-  ##
-  # Responds to GET /
-  #
-  def index
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  root 'landing#index'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post],
         as: 'auth' # used by omniauth
 
   resources :users, param: :username
+
+  match '/', to: 'users#index', via: :all, as: 'root'
+  match '/', to: redirect('/users'), via: :all
 end
